936d08a085882270b060a57c8d9671c91422c80d
Jose Eliseo Espinoza Barrera <eliseoespinoza333@gmail.com>
2024-08-13 19:59:56 -0600

    creacion de index.php con estructural html

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JOSE ELISEO ESPINOZA BARRERA</title>
+        <link rel="stylesheet" href="css/bootstarp.min.css" />
+        <link rel="styleshett" href="css/style.css" />
+        <script>"js/jquery-3.7.1min"</script>
+        <script>"js/bootstrap.bundle.min.js"</script>
+    </head>
+    <body>
+
+    </bocy>
+
+</html>
